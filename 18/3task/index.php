04b79c054c5424eb2f212b0c93cc4ea64eabbf28
Paulius <pauliuspk@gmail.com>
2020-08-18 12:27:37 +0300

    4task. Seconds

--- 18/3task/index.php
+++ 18/3task/index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php print 'As' . ' ' . date("l") . ' ' . 'ir PHP';?></title>
+    <title><?php print 'As' . ' ' . date('l') . ' ' . 'ir PHP';?></title>
 </head>
 <body>
    <h1>Paulius-HTML <?php print('ir PHP ');?> asas <br> jau nuo <?php print date('Y') . ' ' . 'metu';?></h1>
     <p>Viskas prasidejo <?php print date('m') . ' ' . 'menesio' . ' ' . date('d') . ' ' . 'diena' ;?></p>
 </body>
 </html>
+
+
