79ecf07e34d216db7d5c43364700b4629ea5404b
Paulius <pauliuspk@gmail.com>
2020-08-24 21:58:02 +0300

    Login.php created

---
+++ login_signup/home.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+<h1>WELCOME <?php print $_SESSION['username']; ?></h1>
+    
+</body>
+</html>
--- login_signup/validation.php
+++ login_signup/validation.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-header('location:index.php');
+
 
 
 $con = mysqli_connect('phpsualum.lt:3307', 'root', '');
@@ -11,17 +11,16 @@ mysqli_select_db($con, 'userregistration');
 $name = $_POST['user_name'];
 $pass = $_POST['password'];
 
-$s  = " select * from usertable where name = '$name'";
+$s  = " select * from usertable where name = '$name' && password = '$pass'";
 
 $result = mysqli_query($con, $s);
 
 $num = mysqli_num_rows($result);
 
 if ($num === 1) {
-    print "Username is already taken";
+    $_SESSION['username'] = $name;
+   header('location:home.php');
 } else {
+header('location:index.php');
 
-    $reg = " insert into usertable(name, password) values ('$name', '$pass')";
-    mysqli_query($con, $reg);
-    print "Registration successful";
 }
