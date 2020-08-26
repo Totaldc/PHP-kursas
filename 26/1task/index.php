b41b2ce294c489b75362df0396c9d7ba679ade2c
Paulius <pauliuspk@gmail.com>
2020-08-26 10:49:38 +0300

    1task. Square number from form input

---
+++ 26/1task/index.php
@@ -0,0 +1,35 @@
+<!-- 1 uzduotis -->
+
+<?php
+$x = $_POST['number'];
+
+function square($x){
+
+print  $x  * $x;
+
+}
+
+square($x);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form  method="POST">
+
+    <input type="number" name="number" id="name" placeholder="Enter number">
+    <input type="submit">
+
+    </form>
+
+</body>
+</html>
