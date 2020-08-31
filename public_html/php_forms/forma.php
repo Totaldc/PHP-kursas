0513dc7324de5ae1fa22ac300de719166580d6f0
Paulius <pauliuspk@gmail.com>
2020-08-31 12:09:51 +0300

    SANITIZE ARRAY function. APPROVED edition

--- public_html/php_forms/forma.php
+++ public_html/php_forms/forma.php
@@ -1,26 +1,39 @@
 <?php
 
+$fields = ['vardas', 'pavarde'];
+
+/**
+ * sanitize array keys
+ *
+ * @param array $fields
+ * @return array
+ */
 function sanitize_post($fields)
 {
-    foreach ($fields as $key => $value) {
-        $filter[$key] = FILTER_SANITIZE_SPECIAL_CHARS;
+
+    var_dump($fields);
+
+    foreach ($fields as $value) {
+        $filter_params[$value] = FILTER_SANITIZE_SPECIAL_CHARS;
     }
-    return filter_input_array(INPUT_POST, $filter);
+    return filter_input_array(INPUT_POST, $filter_params);
 }
 
-    $input = sanitize_post($_POST);
+$input = sanitize_post($fields);
 
 ?>
 
 <html>
-    <body>
-        <h1>Hack it!</h1>
-        <form method="POST">
-            <input type="text" name="vardas">
-            <input type="text" name="pavarde">
-            <input type="submit">
-        </form>
-        <p><?php print $input['vardas'];?></p>
-        <p><?php print $input['pavarde'];?></p>
-    </body>
+
+<body>
+    <h1>Hack it!</h1>
+    <form method="POST">
+        <input type="text" name="vardas">
+        <input type="text" name="pavarde">
+        <input type="submit">
+    </form>
+    <p><?php print $input['vardas']; ?></p>
+    <p><?php print $input['pavarde']; ?></p>
+</body>
+
 </html>
