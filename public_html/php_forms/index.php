2e1acc35083b301922cfc62f14266999ce0607d9
Paulius <pauliuspk@gmail.com>
2020-09-17 17:40:48 +0300

    Got the right values to variables x y and color

--- public_html/php_forms/index.php
+++ public_html/php_forms/index.php
@@ -12,26 +12,40 @@ $db = new FileDB(DB_FILE);
 $db->load();
 $database = $db->getData();
 
-var_dump($database['coord']);
+foreach ($database['coord'] as $key => $value) {
+	foreach ($value as $key => $item) {
+		if ($key === 'x') {
+			$x = $item;
+		} else if ($key === 'y') {
+			$y = $item;
+		} else if ($key === 'color') {
+			$color = $item;
+		}
+	}
+}
 
+print $x;
+print $y;
+print $color;
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>Index</title>
 </head>
+
 <body>
 	<header>
 		<?php
-		include 'app/templates/nav.php'; 
+		include 'app/templates/nav.php';
 		?>
 	</header>
-<h1><?php print $message; ?></h1>
+	<h1><?php print $message; ?></h1>
 
 </body>
-</html>
 
+</html>
