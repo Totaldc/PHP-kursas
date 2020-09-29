4594e22575f85b5872f4dc1ee470fab9c7e33085
Paulius <pauliuspk@gmail.com>
2020-09-29 20:20:04 +0300

    Add.php seems to work. Every page redone

--- public_html/php_forms/add.php
+++ public_html/php_forms/add.php
@@ -4,10 +4,11 @@ require('bootloader.php');
 
 use App\App;
 use App\Pixels\Pixel;
-use App\Views\Navigation;
 use Core\Views\Form;
+use App\Views\Pages\BasePage;
+use Core\Views\Content;
 
-$nav = new Navigation();
+
 
 if (!App::$session->getUser()) {
 	header('Location: login.php');
@@ -28,31 +29,9 @@ if ($view_add_form->isSubmitted()) {
 	}
 }
 
+$content = new Content();
 
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="assets/css/style.css">
-	<title>Add pixels</title>
-</head>
-<body>
-<header><?php print $nav->render(); ?></header>
-<main>
-	<div class="add container">
-		<h1>Add pixel</h1>
-		<?php print $view_add_form->render(); ?>
-		<?php if (isset($message)) : ?>
-			<div class="message">
-				<span><?php print $message; ?></span>
-			</div>
-		<?php endif; ?>
-	</div>
-</main>
-</body>
-</html>
+$indexPage = new BasePage();
+$indexPage->setTitle('Index: Pixels');
+$indexPage->setContent($content->render('add.tpl.php'));
+print $indexPage->render();
