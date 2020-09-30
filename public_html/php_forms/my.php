1f9f13b3dbdc7aa4c9ad76ad9c635b071737d443
Paulius <pauliuspk@gmail.com>
2020-09-30 11:14:48 +0300

    index controller added + my controller added

--- public_html/php_forms/my.php
+++ public_html/php_forms/my.php
@@ -1,27 +1,8 @@
 <?php
 
+
+use App\Controllers\MyController;
+
 require('bootloader.php');
-use App\App;
-use App\Views\Pages\BasePage;
-use Core\Views\Content;
-
-if (!App::$session->getUser()) {
-	header('Location: login.php');
-	exit;
-}
-
-if (App::$db->tableExists('pixels')) {
-	$pixels = App::$db->getRowsWhere('pixels', ['email' => $_SESSION['email']]);
-}
-
-$content = new Content($pixels);
-
-$indexPage = new BasePage();
-
-$indexPage->setTitle('My Pixels');
-
-$indexPage->setContent($content->render('index.tpl.php'));
-
-print $indexPage->render();
-?>
-
+$controller = new MyController();
+print $controller->index();
